80aa5dab9d14916f99e93f52855030e517f1d75d
MuzykaAndrii <andrymyzik@gmail.com>
2023-07-18 22:43:20 +0300

    implemented user profile model

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="User profile",
+    )
+    name = models.CharField(
+        max_length=50,
+        verbose_name="User name",
+    )
+    second_name = models.CharField(
+        max_length=50,
+        verbose_name="User second name",
+    )
+    surname = models.CharField(
+        max_length=50,
+        verbose_name="User surname",
+        blank=True,
+        null=True,
+    )
+    date_birth = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name="Date of user birth",
+    )
+
+
