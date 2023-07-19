87098c2da02a7291aa92b2985deee4f6b6d17515
MuzykaAndrii <andrymyzik@gmail.com>
2023-07-19 15:18:25 +0300

    merged contact app into user app

--- src/user/models.py
+++ src/user/models.py
@@ -27,5 +27,42 @@ class Profile(models.Model):
         null=False,
         verbose_name="Date of user birth",
     )
+    birth_place = models.CharField(
+        max_length=200,
+        verbose_name="Place of birth",
+    )
+    residence_place = models.CharField(
+        max_length=200,
+        verbose_name="Current live place",
+    )
+
+
+class Contact(models.Model):
+    CONTACT_TYPES = (
+        ('phone', 'Phone number'),
+        ('email', 'Email address'),
+        ('telegram', 'Telegram'),
+        ('viber', 'Viber'),
+        ('linkedin', 'Linkedin'),
+    )
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='contacts',
+        verbose_name="Contact owner",
+    )
+    type = models.CharField(
+        max_length=30,
+        choices=CONTACT_TYPES,
+        blank=False,
+        null=False,
+        verbose_name="Contact type"
+    )
+    link = models.CharField(
+        max_length=200,
+        blank=False,
+        null=False,
+        verbose_name="Link to contact",
+    )
 
 
